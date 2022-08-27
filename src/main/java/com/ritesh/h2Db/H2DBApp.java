e2548d7b9eeceba78448c5435dbb707ca80ec58c
Ritesh Kumar <ritesh705@gmail.com>
2022-08-27 19:20:42 +0530

    update

--- src/main/java/com/ritesh/h2Db/H2DBApp.java
+++ src/main/java/com/ritesh/h2Db/H2DBApp.java
@@ -3,7 +3,10 @@ package com.ritesh.h2Db;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/*
+@SpringBootApplication is equivalent to using
+@Configuration, @EnableAutoConfiguration, and @ComponentScan
+*/
 @SpringBootApplication
 public class
 H2DBApp {
